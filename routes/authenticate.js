fb958a41974dde5f9c036705158025ffa46d0695
TylerMizuyabu <mizuyabu@hotmail.ca>
2016-11-14 23:09:24 -0500

    removed attempt to allow CORS

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,29 +2,11 @@ var router = require('express').Router();
 
 module.exports = function(passport){
 
-    router.use(function(req,res,next){
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
-        next();
-    });
-
     router.get('/success', function(req,res,next){
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         res.send({status:'success', user: req.user ? req.user:null});
     });
 
     router.get('/failure', function(req,res,next){
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         res.send({status:'failure', user:null});
     });
     
